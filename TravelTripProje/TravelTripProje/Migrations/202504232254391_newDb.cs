92dffda34c6ba561c17599c93480ba11b097cae3
yusuf439 <yusuf439000@gmail.com>
2025-05-06 14:04:29 +0300

    Tatil Seyahat Blog Sitesi
    Bu site MVC5 ile geştirilmiştir. Projenin konsepti ise tatil yapan kişinin deneyimlerini kendi sitesinde yayınlayabilmesidir.

---
+++ TravelTripProje/TravelTripProje/Migrations/202504232254391_newDb.cs
@@ -0,0 +1,96 @@
+﻿namespace TravelTripProje.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Kullanici = c.String(),
+                        Sifre = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.AdresBlogs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Baslik = c.String(),
+                        Aciklama = c.String(),
+                        AdresAcik = c.String(),
+                        Mail = c.String(),
+                        Telefon = c.String(),
+                        Konum = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Baslik = c.String(),
+                        Tarih = c.DateTime(nullable: false),
+                        Aciklama = c.String(),
+                        BlogImage = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Yorumlars",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        KullaniciAdi = c.String(),
+                        Mail = c.String(),
+                        Yorum = c.String(),
+                        Blog_ID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Blogs", t => t.Blog_ID)
+                .Index(t => t.Blog_ID);
+            
+            CreateTable(
+                "dbo.Hakkimizdas",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FotoUrl = c.String(),
+                        Aciklama = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.iletisims",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        AdSoyad = c.String(),
+                        Mail = c.String(),
+                        Konu = c.String(),
+                        Mesaj = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Yorumlars", "Blog_ID", "dbo.Blogs");
+            DropIndex("dbo.Yorumlars", new[] { "Blog_ID" });
+            DropTable("dbo.iletisims");
+            DropTable("dbo.Hakkimizdas");
+            DropTable("dbo.Yorumlars");
+            DropTable("dbo.Blogs");
+            DropTable("dbo.AdresBlogs");
+            DropTable("dbo.Admins");
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Siniflar/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string Kullanici { get; set; }
+        public int Sifre { get; set; }
+    }
+}
